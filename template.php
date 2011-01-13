c89a8ae16f5725169f12af5516c3fcb3c5adb11d
Capi /. Etheriel <barraponto@gmail.com>
2011-01-13 18:07:36 -0200

    starting git development

---
+++ template.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Override or insert PHPTemplate variables into the templates.
+*/
+function thacker_preprocess_page(&$vars) {
+
+  // Add per content type pages
+    if (isset($vars['node'])) {
+
+	  // Add template naming suggestion. It should alway use hyphens.
+	    // If node type is "custom_news", it will pickup "page-custom-news.tpl.php".
+		  $vars['template_files'][] = 'page-'. str_replace('_', '-', $vars['node']->type);
+		    }
+
+			}
+?>
