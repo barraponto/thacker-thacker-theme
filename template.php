a15a64974e843e71b24e78dce3ad528ed2bbf080
Capi Etheriel <barraponto@gmail.com>
2011-03-15 22:47:43 -0300

    getting rid of taxonomies in projects

--- template.php
+++ template.php
@@ -3,16 +3,16 @@
 * Override or insert PHPTemplate variables into the templates.
 */
 function thacker_preprocess_page(&$vars) {
-
   // Add per content type pages
-    if (isset($vars['node'])) {
+  if (isset($vars['node'])) {
+    // Add template naming suggestion. It should alway use hyphens.
+    // If node type is "custom_news", it will pickup "page-custom-news.tpl.php".
+    $vars['template_files'][] = 'page-'. str_replace('_', '-', $vars['node']->type);
+  }
+}
 
-	  // Add template naming suggestion. It should alway use hyphens.
-	    // If node type is "custom_news", it will pickup "page-custom-news.tpl.php".
-		  $vars['template_files'][] = 'page-'. str_replace('_', '-', $vars['node']->type);
-		    }
-
-			}
-
-
-?>
+function thacker_preprocess_node(&$vars) {
+  if ($vars['node']->type == 'dt_project') {
+    $vars['terms'] = FALSE;
+  }
+}
